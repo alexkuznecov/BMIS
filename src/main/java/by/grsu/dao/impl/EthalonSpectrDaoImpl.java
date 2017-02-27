d984cb2367bf6f2c2088a3f259c2fad81aff450d
Alex Kuznecov <kuzniecov@mail.ru>
2017-02-27 03:47:26 +0300

    #19 - Реализовать логику работы с бд для класса EthalonSpectr

--- src/main/java/by/grsu/dao/impl/EthalonSpectrDaoImpl.java
+++ src/main/java/by/grsu/dao/impl/EthalonSpectrDaoImpl.java
@@ -3,6 +3,9 @@ package by.grsu.dao.impl;
 import by.grsu.dao.AbstractDao;
 import by.grsu.dao.EthalonSpectrDao;
 import by.grsu.entity.EthalonSpectr;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,26 +18,32 @@ public class EthalonSpectrDaoImpl extends AbstractDao implements EthalonSpectrDa
 
     @Override
     public void saveEthalonSpectr(EthalonSpectr ethalonSpectr) {
-
+        persist(ethalonSpectr);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<EthalonSpectr> findAllEthalonSpectrs() {
-        return null;
+        Criteria criteria = getSession().createCriteria(EthalonSpectr.class);
+        return (List<EthalonSpectr>) criteria.list();
     }
 
     @Override
     public void deleteEthalonSpectrById(Integer etsid) {
-
+        Query query = getSession().createSQLQuery("delete from EthalonSpectr where etsid = :etsid");
+        query.setInteger("etsid", etsid);
+        query.executeUpdate();
     }
 
     @Override
     public EthalonSpectr findById(Integer etsid) {
-        return null;
+        Criteria criteria = getSession().createCriteria(EthalonSpectr.class);
+        criteria.add(Restrictions.eq("etsid",etsid));
+        return (EthalonSpectr) criteria.uniqueResult();
     }
 
     @Override
     public void updateEthalonSpectr(EthalonSpectr ethalonSpectr) {
-
+        getSession().update(ethalonSpectr);
     }
 }

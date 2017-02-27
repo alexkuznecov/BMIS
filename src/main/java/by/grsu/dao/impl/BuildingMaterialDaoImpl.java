b59ef3db5bba2a460cdac6f09d250ec5862b4c0c
Alex Kuznecov <kuzniecov@mail.ru>
2017-02-27 03:41:28 +0300

    #18 - Реализовать логику работы с бд для класса BuildingMaterial

--- src/main/java/by/grsu/dao/impl/BuildingMaterialDaoImpl.java
+++ src/main/java/by/grsu/dao/impl/BuildingMaterialDaoImpl.java
@@ -3,6 +3,9 @@ package by.grsu.dao.impl;
 import by.grsu.dao.AbstractDao;
 import by.grsu.dao.BuildingMaterialDao;
 import by.grsu.entity.BuildingMaterial;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,26 +18,32 @@ public class BuildingMaterialDaoImpl extends AbstractDao implements BuildingMate
 
     @Override
     public void saveBuildingMaterial(BuildingMaterial buildingMaterial) {
-
+        persist(buildingMaterial);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<BuildingMaterial> findAllBuildingMaterials() {
-        return null;
+        Criteria criteria = getSession().createCriteria(BuildingMaterial.class);
+        return (List<BuildingMaterial>) criteria.list();
     }
 
     @Override
     public void deleteBuildingMaterialById(Integer bmid) {
-
+        Query query = getSession().createSQLQuery("delete from BuildingMaterial where bmid = :bmid");
+        query.setInteger("bmid", bmid);
+        query.executeUpdate();
     }
 
     @Override
     public BuildingMaterial findById(Integer bmid) {
-        return null;
+        Criteria criteria = getSession().createCriteria(BuildingMaterial.class);
+        criteria.add(Restrictions.eq("bmid",bmid));
+        return (BuildingMaterial) criteria.uniqueResult();
     }
 
     @Override
     public void updateBuildingMaterial(BuildingMaterial buildingMaterial) {
-
+        getSession().update(buildingMaterial);
     }
 }

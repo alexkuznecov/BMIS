0318aa83f52ae956733ae7169d03eda5046eae0e
Alex Kuznecov <kuzniecov@mail.ru>
2017-02-27 03:53:12 +0300

    #20 - Реализовать логику работы с бд для класса Manufacturer

--- src/main/java/by/grsu/dao/impl/ManufacturerDaoImpl.java
+++ src/main/java/by/grsu/dao/impl/ManufacturerDaoImpl.java
@@ -3,6 +3,9 @@ package by.grsu.dao.impl;
 import by.grsu.dao.AbstractDao;
 import by.grsu.dao.ManufacturerDao;
 import by.grsu.entity.Manufacturer;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,26 +18,32 @@ public class ManufacturerDaoImpl extends AbstractDao implements ManufacturerDao 
 
     @Override
     public void saveManufacturer(Manufacturer manufacturer) {
-
+        persist(manufacturer);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<Manufacturer> findAllManufacturers() {
-        return null;
+        Criteria criteria = getSession().createCriteria(Manufacturer.class);
+        return (List<Manufacturer>) criteria.list();
     }
 
     @Override
     public void deleteManufacturerById(Integer mnfid) {
-
+        Query query = getSession().createSQLQuery("delete from Manufacturer where mnfid = :mnfid");
+        query.setInteger("mnfid", mnfid);
+        query.executeUpdate();
     }
 
     @Override
     public Manufacturer findById(Integer mnfid) {
-        return null;
+        Criteria criteria = getSession().createCriteria(Manufacturer.class);
+        criteria.add(Restrictions.eq("mnfid",mnfid));
+        return (Manufacturer) criteria.uniqueResult();
     }
 
     @Override
     public void updateManufacturer(Manufacturer manufacturer) {
-
+        getSession().update(manufacturer);
     }
 }

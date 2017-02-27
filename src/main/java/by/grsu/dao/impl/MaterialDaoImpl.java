51bb5d2ef3e564c38809310ee6834dfc75c02d46
Alex Kuznecov <kuzniecov@mail.ru>
2017-02-27 03:57:10 +0300

    #21 - Реализовать логику работы с бд для класса Material

--- src/main/java/by/grsu/dao/impl/MaterialDaoImpl.java
+++ src/main/java/by/grsu/dao/impl/MaterialDaoImpl.java
@@ -3,6 +3,9 @@ package by.grsu.dao.impl;
 import by.grsu.dao.AbstractDao;
 import by.grsu.dao.MaterialDao;
 import by.grsu.entity.Material;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,26 +18,32 @@ public class MaterialDaoImpl extends AbstractDao implements MaterialDao {
 
     @Override
     public void saveMaterial(Material material) {
-
+        persist(material);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<Material> findAllMaterials() {
-        return null;
+        Criteria criteria = getSession().createCriteria(Material.class);
+        return (List<Material>) criteria.list();
     }
 
     @Override
     public void deleteMaterialById(Integer mid) {
-
+        Query query = getSession().createSQLQuery("delete from Material where mid = :mid");
+        query.setInteger("mid", mid);
+        query.executeUpdate();
     }
 
     @Override
     public Material findById(Integer mid) {
-        return null;
+        Criteria criteria = getSession().createCriteria(Material.class);
+        criteria.add(Restrictions.eq("mid",mid));
+        return (Material) criteria.uniqueResult();
     }
 
     @Override
     public void updateMaterial(Material material) {
-
+        getSession().update(material);
     }
 }

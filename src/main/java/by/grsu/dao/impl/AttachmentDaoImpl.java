cc0d32fdcd9df5eb5e31de1d09639cda328b643d
Alex Kuznecov <kuzniecov@mail.ru>
2017-02-27 03:33:09 +0300

    #17  - Реализовать логику работы с бд для класса Attachment

--- src/main/java/by/grsu/dao/impl/AttachmentDaoImpl.java
+++ src/main/java/by/grsu/dao/impl/AttachmentDaoImpl.java
@@ -3,6 +3,9 @@ package by.grsu.dao.impl;
 import by.grsu.dao.AbstractDao;
 import by.grsu.dao.AttachmentDao;
 import by.grsu.entity.Attachment;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,26 +18,32 @@ public class AttachmentDaoImpl extends AbstractDao implements AttachmentDao {
 
     @Override
     public void saveAttachment(Attachment attachment) {
-
+        persist(attachment);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<Attachment> findAllAttachments() {
-        return null;
+        Criteria criteria = getSession().createCriteria(Attachment.class);
+        return (List<Attachment>) criteria.list();
     }
 
     @Override
     public void deleteAttachmentById(Integer attchid) {
-
+        Query query = getSession().createSQLQuery("delete from Attachment where attchid = :attchid");
+        query.setInteger("attchid", attchid);
+        query.executeUpdate();
     }
 
     @Override
     public Attachment findById(Integer attchid) {
-        return null;
+        Criteria criteria = getSession().createCriteria(Attachment.class);
+        criteria.add(Restrictions.eq("attchid",attchid));
+        return (Attachment) criteria.uniqueResult();
     }
 
     @Override
     public void updateAttachment(Attachment attachment) {
-
+        getSession().update(attachment);
     }
 }

82575ad9e05a755510a8d92a383958791888155a
Alex Kuznecov <kuzniecov@mail.ru>
2017-03-01 23:05:51 +0300

    #28 - Реализовать логику работы с бд для класса ResearchObjectType

--- src/main/java/by/grsu/dao/impl/ResearchObjectTypeDaoImpl.java
+++ src/main/java/by/grsu/dao/impl/ResearchObjectTypeDaoImpl.java
@@ -3,6 +3,9 @@ package by.grsu.dao.impl;
 import by.grsu.dao.AbstractDao;
 import by.grsu.dao.ResearchObjectTypeDao;
 import by.grsu.entity.ResearchObjectType;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,26 +18,32 @@ public class ResearchObjectTypeDaoImpl extends AbstractDao implements ResearchOb
 
     @Override
     public void saveResearchObjectType(ResearchObjectType researchObjectType) {
-
+        persist(researchObjectType);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<ResearchObjectType> findAllResearchObjectTypes() {
-        return null;
+        Criteria criteria = getSession().createCriteria(ResearchObjectType.class);
+        return (List<ResearchObjectType>) criteria.list();
     }
 
     @Override
     public void deleteResearchObjectTypeById(Integer rotid) {
-
+        Query query = getSession().createSQLQuery("delete from ResearchObjectType where rotid = :rotid");
+        query.setInteger("rotid", rotid);
+        query.executeUpdate();
     }
 
     @Override
     public ResearchObjectType findById(Integer rotid) {
-        return null;
+        Criteria criteria = getSession().createCriteria(ResearchObjectType.class);
+        criteria.add(Restrictions.eq("rotid",rotid));
+        return (ResearchObjectType) criteria.uniqueResult();
     }
 
     @Override
     public void updateResearchObjectType(ResearchObjectType researchObjectType) {
-
+        getSession().update(researchObjectType);
     }
 }

84ae731c3501d25739e1f1d44b4580e80a934142
Alex Kuznecov <kuzniecov@mail.ru>
2017-03-01 23:13:14 +0300

    #30 - Реализовать логику работы с бд для класса Spectr

--- src/main/java/by/grsu/dao/impl/SpectrDaoImpl.java
+++ src/main/java/by/grsu/dao/impl/SpectrDaoImpl.java
@@ -3,6 +3,9 @@ package by.grsu.dao.impl;
 import by.grsu.dao.AbstractDao;
 import by.grsu.dao.SpectrDao;
 import by.grsu.entity.Spectr;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,26 +18,32 @@ public class SpectrDaoImpl extends AbstractDao implements SpectrDao {
 
     @Override
     public void saveSpectr(Spectr spectr) {
-
+        persist(spectr);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<Spectr> findAllSpectrs() {
-        return null;
+        Criteria criteria = getSession().createCriteria(Spectr.class);
+        return (List<Spectr>) criteria.list();
     }
 
     @Override
     public void deleteSpectrById(Integer spcid) {
-
+        Query query = getSession().createSQLQuery("delete from Spectr where spcid = :spcid");
+        query.setInteger("spcid", spcid);
+        query.executeUpdate();
     }
 
     @Override
     public Spectr findById(Integer spcid) {
-        return null;
+        Criteria criteria = getSession().createCriteria(Spectr.class);
+        criteria.add(Restrictions.eq("spcid",spcid));
+        return (Spectr) criteria.uniqueResult();
     }
 
     @Override
     public void updateSpectr(Spectr spectr) {
-
+        getSession().update(spectr);
     }
 }

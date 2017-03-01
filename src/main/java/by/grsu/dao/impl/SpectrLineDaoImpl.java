58ab15ef30e29fed5e686551a9d5db012147c7b2
Alex Kuznecov <kuzniecov@mail.ru>
2017-03-01 23:17:13 +0300

    #31 - Реализовать логику работы с бд для класса SpectrLine

--- src/main/java/by/grsu/dao/impl/SpectrLineDaoImpl.java
+++ src/main/java/by/grsu/dao/impl/SpectrLineDaoImpl.java
@@ -3,6 +3,9 @@ package by.grsu.dao.impl;
 import by.grsu.dao.AbstractDao;
 import by.grsu.dao.SpectrLineDao;
 import by.grsu.entity.SpectrLine;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,26 +18,32 @@ public class SpectrLineDaoImpl extends AbstractDao implements SpectrLineDao{
 
     @Override
     public void saveSpectrLine(SpectrLine spectrLine) {
-
+        persist(spectrLine);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<SpectrLine> findAllSpectrLines() {
-        return null;
+        Criteria criteria = getSession().createCriteria(SpectrLine.class);
+        return (List<SpectrLine>) criteria.list();
     }
 
     @Override
     public void deleteSpectrLineById(Integer slid) {
-
+        Query query = getSession().createSQLQuery("delete from SpectrLine where slid = :slid");
+        query.setInteger("slid", slid);
+        query.executeUpdate();
     }
 
     @Override
     public SpectrLine findById(Integer slid) {
-        return null;
+        Criteria criteria = getSession().createCriteria(SpectrLine.class);
+        criteria.add(Restrictions.eq("slid",slid));
+        return (SpectrLine) criteria.uniqueResult();
     }
 
     @Override
     public void updateSpectrLine(SpectrLine spectrLine) {
-
+        getSession().update(spectrLine);
     }
 }

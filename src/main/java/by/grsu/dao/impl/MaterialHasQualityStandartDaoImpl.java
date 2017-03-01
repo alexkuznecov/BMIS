cfd338678ea1bdf1c862ef9f947e70f65294bf79
Alex Kuznecov <kuzniecov@mail.ru>
2017-03-01 22:41:20 +0300

    #23 - Реализовать логику работы с бд для класса MAterialHasQualityStandart

--- src/main/java/by/grsu/dao/impl/MaterialHasQualityStandartDaoImpl.java
+++ src/main/java/by/grsu/dao/impl/MaterialHasQualityStandartDaoImpl.java
@@ -3,6 +3,9 @@ package by.grsu.dao.impl;
 import by.grsu.dao.AbstractDao;
 import by.grsu.dao.MaterialHasQualityStandartDao;
 import by.grsu.entity.MaterialHasQualityStandart;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,26 +18,32 @@ public class MaterialHasQualityStandartDaoImpl extends AbstractDao implements Ma
 
     @Override
     public void saveMaterialHasQualityStandart(MaterialHasQualityStandart materialHasQualityStandart) {
-
+        persist(materialHasQualityStandart);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<MaterialHasQualityStandart> findAllMaterialHasQualityStandarts() {
-        return null;
+        Criteria criteria = getSession().createCriteria(MaterialHasQualityStandart.class);
+        return (List<MaterialHasQualityStandart>) criteria.list();
     }
 
     @Override
     public void deleteMaterialHasQualityStandartById(Integer id) {
-
+        Query query = getSession().createSQLQuery("delete from Material_has_QualityStandart where id = :id");
+        query.setInteger("id", id);
+        query.executeUpdate();
     }
 
     @Override
     public MaterialHasQualityStandart findById(Integer id) {
-        return null;
+        Criteria criteria = getSession().createCriteria(MaterialHasQualityStandart.class);
+        criteria.add(Restrictions.eq("id",id));
+        return (MaterialHasQualityStandart) criteria.uniqueResult();
     }
 
     @Override
     public void updateMaterialHasQualityStandart(MaterialHasQualityStandart materialHasQualityStandart) {
-
+        getSession().update(materialHasQualityStandart);
     }
 }

8b08ea0addef37af2ac1cd8c4135175d2b693408
Alex Kuznecov <kuzniecov@mail.ru>
2017-03-01 22:46:17 +0300

    #24 - Реализовать логику работы с бд для класса Organization

--- src/main/java/by/grsu/dao/impl/OrganizationDaoImpl.java
+++ src/main/java/by/grsu/dao/impl/OrganizationDaoImpl.java
@@ -3,6 +3,9 @@ package by.grsu.dao.impl;
 import by.grsu.dao.AbstractDao;
 import by.grsu.dao.OrganizationDao;
 import by.grsu.entity.Organization;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,26 +18,32 @@ public class OrganizationDaoImpl extends AbstractDao implements OrganizationDao 
 
     @Override
     public void saveOrganization(Organization organization) {
-
+        persist(organization);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<Organization> findAllOrganizations() {
-        return null;
+        Criteria criteria = getSession().createCriteria(Organization.class);
+        return (List<Organization>) criteria.list();
     }
 
     @Override
     public void deleteOrganizationById(Integer oid) {
-
+        Query query = getSession().createSQLQuery("delete from Material where oid = :oid");
+        query.setInteger("oid", oid);
+        query.executeUpdate();
     }
 
     @Override
     public Organization findById(Integer oid) {
-        return null;
+        Criteria criteria = getSession().createCriteria(Organization.class);
+        criteria.add(Restrictions.eq("oid",oid));
+        return (Organization) criteria.uniqueResult();
     }
 
     @Override
     public void updateOrganization(Organization organization) {
-
+        getSession().update(organization);
     }
 }
